8775d33ebaf0197c9d0d997c560b450f8c03c1d6
Zahid Musthofainal Akhyar <zahidakhyar@gmail.com>
2019-11-19 21:56:24 +0700

    Commit Model

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,12 +1,13 @@
 <?php
-    
     defined('BASEPATH') OR exit('No direct script access allowed');
     
     class User extends CI_Controller {
     
-        public function index() {
+        public function __construct() {
+            parent::__construct();
             
         }
+        
     
     }
 ?>
