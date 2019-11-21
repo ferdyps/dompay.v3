5bdecacc0db2ff31d7b9275520b220c587321e3c
ferdyps <fpittardi@gmail.com>
2019-11-21 12:53:46 +0700

    Update bank.php

--- application/views/user/bank.php
+++ application/views/user/bank.php
@@ -1,11 +1,11 @@
 <div class="container-fluid">
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800"><i class="far fa-user fa-fw"></i> Akun</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-primary shadow-sm"  data-toggle="modal" data-target="#addAccountModal"><i class="fas fa-plus-circle fa-sm"></i> Tambah Akun</a>
+        <h1 class="h3 mb-0 text-gray-800"><i class="fas fa-university fa-fw"></i> Bank</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-primary shadow-sm"  data-toggle="modal" data-target="#addAccountModal"><i class="fas fa-plus-circle fa-sm"></i> Tambah Akun Bank</a>
     </div>
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="mr-0 font-weight-bold text-primary">Data Akun</h6>
+            <h6 class="mr-0 font-weight-bold text-primary">Data Akun Bank</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
@@ -13,9 +13,9 @@
                     <thead>
                         <tr class="text-center bg-primary text-white">
                             <th width="5%">No</th>
-                            <th width="5%">Id</th>
-                            <th>Username</th>
-                            <th>Akses</th>
+                            <th width="5%">Username</th>
+                            <th>Nomor Rekening</th>
+                            <th>Tipe Bank</th>
                             <th width="10%">Action</th>
                         </tr>
                     </thead>
