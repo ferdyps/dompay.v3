ea672c9c26b3513a843300d681d5ff819b87ee12
ferdyps <fpittardi@gmail.com>
2019-11-21 14:17:38 +0700

    ubah title sama gambarnya

--- application/views/auth/_partials/header.php
+++ application/views/auth/_partials/header.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title><?= config_item('web_title'); ?></title>
+        <link rel="shortcut icon" href="<?= base_url('assets/images/login.png');?>" type="image/x-icon">
         <link rel="stylesheet" href="<?= base_url('assets/vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css'); ?>">
         <link rel="stylesheet" href="<?= base_url('assets/vendor/fontawesome-free-5.7.2-web/css/all.min.css'); ?>">
         <link rel="stylesheet" href="<?= base_url('assets/css/iofrm-style.css'); ?>">
