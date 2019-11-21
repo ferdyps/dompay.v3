950febf5adda62828c8d0876cee7041f032640e8
ferdyps <fpittardi@gmail.com>
2019-11-21 13:28:22 +0700

    Create mutasi.php

---
+++ application/views/user/mutasi.php
@@ -0,0 +1,27 @@
+<div class="container-fluid">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><i class="fas fa-sync-alt fa-fw"></i> Mutasi</h1>
+    </div>
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="mr-0 font-weight-bold text-primary">Data Mutasi</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped table-hover" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr class="text-center bg-primary text-white">
+                            <th width="5%">No</th>
+                            <th>Nomor Rekening</th>
+                            <th>Tanggal Mutasi</th>
+                            <th>Nominal</th>
+                            <th>Tipe Mutasi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
